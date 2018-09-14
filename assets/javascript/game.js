70eb16b0aff4afb32a87f2ab5a782f696a4ddf9f
hadiburpee <hadiburpee@gmail.com>
2018-09-13 18:45:59 -0700

    glob var and stuff

---
+++ assets/javascript/game.js
@@ -0,0 +1,48 @@
+//GLOBAL VARIABLES
+
+var Luke = {name: "Luke_Skywalker", hp: 150, ap: 7, ca: 7};
+var Vader = {name: "Darth_Vader", hp: 200, ap: 10, ca: 20};
+var Solo = {name: "Han_Solo", hp: 125, ap: 5, ca: 25};
+var Lando = {name: "Lando_Calrissian", hp: 175, ap: 6, ca: 15};
+var playerChoice;
+var enemy1;
+var enemy2;
+var enemy3;
+var wins = 0;
+var loses = 0;
+
+//Code within the ready will only run once the DOM is ready.
+$(document).ready(function(){
+
+
+
+console.log(Lando.name);
+console.log(Lando.ca); 
+
+console.log(Lando.hp - Luke.ca);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
