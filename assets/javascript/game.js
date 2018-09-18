27d506b42635c0eeda0212a66294dedf25739f76
hadiburpee <hadiburpee@gmail.com>
2018-09-17 18:33:00 -0700

    fixed some things

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,19 +19,6 @@ var incrementAP = 0;
 //Code within the ready will only run once the DOM is ready.
 $(document).ready(function(){
 
-    //testing stuff
-// startingAP = Lando.ap
-// console.log(Lando.name);
-// console.log(Lando.ca); 
-// console.log("Lukes HP" + Luke.hp);
-// console.log(Luke.hp -= Lando.ap);
-// Lando.ap += startingAP;
-
-// console.log(Lando.ap);
-// console.log("Lukes HP" + Luke.hp);
-// console.log(Luke.hp -= Lando.ap);
-
-
 //Can I grab the html and move it to another class? yes I can.
 $(".player").click(function(){
     console.log($(this).attr("value"));
@@ -46,7 +33,6 @@ $(".player").click(function(){
     //Choosing Character
     if(playerChoice =="Lando"){
         playerChoice = Lando;
-        console.log(landoPic);
         $(".yourPlayer").append(landoPic);
         $(".enemiesToAttack").append(lukePic, soloPic, vaderPic);
         
@@ -74,30 +60,33 @@ $(".player").click(function(){
     }    
     startingAP = playerChoice.ap;
     // incrementAP = playerChoice.ap;
-    $(".playersToChoose").hide();
+    // $(".playersToChoose").hide();
     
+}); //end of playerchoose click
 
-//choose enemy    
+    //choose enemy    
 $(".player").click(function(){
 
     enemy = $(this).attr("value");
-    var landoPic = $(".landoP");
-    var lukePic = $(".lukeP");
-    var soloPic = $(".soloP");
-    var vaderPic = $(".vaderP");
+    var landoPic = $(".enemiesToAttack .landoP");
+    var lukePic = $(".enemiesToAttack .lukeP");
+    var soloPic = $(".enemiesToAttack .soloP");
+    var vaderPic = $(".enemiesToAttack .vaderP");
     
-    console.log("Enemy is " + enemy);
+    console.log("Enemy is " + enemy);  
+
     if(enemy == "Lando"){
         enemy = Lando;
         $(".defender").append(landoPic);
-        $(".enemiesToAttack .lando").hide();
+        // $(".enemiesToAttack .lando").hide();
         //need to hide
     }
 
     if(enemy == "Luke"){
-        enemy = Luke;        
+        enemy = Luke;
+             
         $(".defender").append(lukePic);
-        $(".enemiesToAttack .luke").hide();
+        // $(".enemiesToAttack .luke").hide();
         //need to hide name and HP too
     }
 
@@ -110,76 +99,32 @@ $(".player").click(function(){
 
     if(enemy == "Vader"){
         enemy = Vader;
-        enemy
         
         $(".defender").append(vaderPic);
         //need to hide 
     }
 
-});
+    });//End of Enemy c
 
-//Attack enemy
+    //Attack enemy
 $(".attackBtn").click(function(){
+    enemy.hp-=startingAP;
+    startingAP+=playerChoice.ap;
+    playerChoice.hp-=enemy.ca;
 
-    //need to fix attacking
-    // enemy.hp-=startingAP;
-    // startingAP += playerChoice.ap;
-    // playerChoice.hp-=enemy.ca;
-    // $(".yourPlayer .hp").html(playerChoice.hp);
-    // $(".defender .hp").html(enemy.hp);
+    console.log("enemy HP" + enemy.hp);
+    console.log("player AP" + playerChoice.ap);
+    console.log("player HP" + playerChoice.hp);
 
-
-    // //write code to change HP?
-
-    // if(playerChoice.hp <= 0){
-    //     //set up reset game function
-    //     loss = loss+1;
-    //     rst();
-    // }
-
-    // else if(enemy.hp <= 0){
-    //     $(".defender").html(" ");
-    //     //replace with next Enemy
-    // }
-
-    // console.log("Lando AP" + startingAP);
-    // console.log("Enemy HP" + enemy.hp);
-    // console.log("Lando HP" + playerChoice.hp);
-
-});
-
-//this resets the game to zero if the player wins or loses.
-function rst(){
+    });
 
 
 
-};
 
-
-});
+// }); //end of first click function
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
+}); //end of document ready
